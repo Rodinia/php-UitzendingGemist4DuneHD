e73d8209ca5a05da208207a37880cc410a37a12a
Rodinia <rodinia@xs4all.nl>
2012-11-01 11:23:18 +0100

    Automatic generation of dune_folder.txt

---
+++ uitzendinggemist/dune/dunefolder.php
@@ -0,0 +1,8 @@
+<?php
+    print("# Reference to Dune HD PHP script for Uitzending Gemist\n");
+    # media_url = dune_http://uitzendinggemist.zxq.net/dune/  
+
+    header('Content-type: text/plain; charset=utf-8');
+    header('Content-Disposition: attachment; filename="dune_folder.txt"');
+    print('dune_http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['PHP_SELF']).'/');
+?>
