841e3bbb3133b39962afa907e2fdb2745e4fc698
Rodina <rodina@xs4all.nl>
2012-11-04 17:19:25 +0100

    Added new index for HTML pages

---
+++ uitzendinggemist/html/index.php
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <title>Uitzending Gemist</title>
+	  <link href="application.css" media="screen" rel="stylesheet" type="text/css" />
+   </head>
+
+   <body>
+      <h1><img src="png/ug-header-logo.png" width="50"/>Uitzending Gemist</h1>
+      <h2>HTML Pages</h2>
+      <table>
+		<tr><td><a href="favorieten.php">Favorieten</a/></td></tr>
+		<tr><td><a href="programmas.php">Uitzending Gemist: Programma's A-Z</a/></td></tr>
+		<tr><td><a href="vara_gemist.php">VARA Gemist</a/></td></tr>
+	  </table>
+
+
+</html>
--- uitzendinggemist/index.php
+++ uitzendinggemist/index.php
@@ -1,17 +1,14 @@
 <html>
    <head>
-      <title>
-         Uitzending Gemist
-      </title>
+      <title>Dune HD Uitzending Gemist</title>
+	  <link href="html/application.css" media="screen" rel="stylesheet" type="text/css" />
    </head>
 
    <body>
       <h1><img src="html/png/ug-header-logo.png" width="50" />Uitzending Gemist</h1>
       <h2>HTML Pages</h2>
       <table>
-		<tr><td><a href="html/">Uitzending Gemist: Favoriete Programma's</a/></td></tr>
-		<tr><td><a href="html/programmas.php">Uitzending Gemist: Programma's A-Z</a/></td></tr>
-		<tr><td><a href="html/vara_gemist.php">VARA Gemist</a/></td></tr>
+		<tr><td><a href="html/">HTML Pages</a/></td></tr>
 	  </table>
 
       <h2>Dune HD pagina's</h2>
